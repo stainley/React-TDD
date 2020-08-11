083c716136ec73744546e9cae1929ab46f435660
Stainley Lebron <saiyamans@gmail.com>
2020-08-11 12:01:43 -0400

    Refactor test

---
+++ test/Appointment.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment } from '../src/Appointment';
+
+
+describe('Appointment', () => {
+
+    let container;
+    let customer;
+    const render = component => ReactDOM.render(component, container);
+
+    beforeEach(() => {
+        container = document.createElement('div');
+    });
+
+    it('renders the customer first name', () => {
+        customer = {firstName: 'Ashley'};                
+        
+        render(<Appointment customer={customer} />);
+        expect(container.textContent).toMatch('Ashley');
+    });
+
+    it('renders another customer first name', () => {
+        customer = {firstName: 'Jordan'};                        
+        render(<Appointment customer={customer} />)        
+
+        expect(container.textContent).toMatch('Jordan');
+    });
+});
