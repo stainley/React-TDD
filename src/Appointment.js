6f09fcf748f0328a44713cffd8bc25ac0c1ed041
Stainley Lebron <saiyamans@gmail.com>
2020-08-11 19:39:59 -0400

    AppointmentDayView

--- src/Appointment.js
+++ src/Appointment.js
@@ -3,3 +3,26 @@ import React from 'react';
 export const Appointment = ({ customer: { firstName}}) => (
     <div>{firstName}</div>
 );
+
+export const AppointmentsDayView = ({appointments}) => (
+    <div id="appointmentsDayView">
+        <ol>
+            {appointments.map(appointment => (
+                    <li key={appointment.startsAt}>
+                        {appointmentTimeOfDay(appointment.startsAt)}
+                    </li>
+                ))}
+        </ol>
+        {appointments.length === 0 ? (
+            <p>There are no appointments scheduled for today.</p>
+        ): (
+            <Appointment {...appointments[0]} />
+        )}
+        
+    </div>
+);
+
+const appointmentTimeOfDay = startsAt => {
+    const [h, m] = new Date(startsAt).toTimeString().split(':');
+    return `${h}:${m}`;
+}
